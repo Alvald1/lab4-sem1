2f7b4d282a074ffd4d087e545e67211324be7ddc
Alvaldi <kirill.a.gribov@gmail.com>
2023-12-01 14:15:41 +0300

    заработал ввод строк, но иногда выводит дополнительный символ %

--- main.c
+++ main.c
@@ -1,41 +1,46 @@
-#include <errno.h>
 #include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define PROMT "$ "
+#define PROMPT "$ "
 
-enum { BAD_ALLOC = 1,
-    OK };
+enum {
+    BAD_ALLOC = 1,
+    OK
+};
 
 int append(char*** words, int* cnt_words, char* word);
 void print(char** words, int cnt_words);
 
 int main()
 {
-    char *line, *word = NULL;
-    char** words = NULL;
-    int cnt_words = 0;
-    while ((line = word = readline(PROMT))) {
-        while (word && (word = strtok(word, " \t"))) {
-            if (append(&words, &cnt_words, word) == BAD_ALLOC) {
-                break;
-            }
-            word += strlen(word) + 1;
+    char *line, *lines = NULL;
+    size_t lines_len;
+    while ((line = readline(PROMPT))) {
+        lines_len = lines ? strlen(lines) + 1 : 0;
+        char* new_lines = (char*)realloc(lines, (lines_len + strlen(line) + 1) * sizeof(char));
+        if (new_lines == NULL) {
+            free(line);
+            free(lines);
+            return 0;
         }
-        print(words, cnt_words);
-        free(words);
+        lines = new_lines;
+        memcpy(lines + lines_len, line, strlen(line) + 1);
+        if (lines_len) {
+            *(lines + lines_len - 1) = '\n';
+        }
         free(line);
-        words = NULL;
     }
+    printf("%s", lines);
+    free(lines);
     return 0;
 }
 
 int append(char*** words, int* cnt_words, char* word)
 {
     char** new_array = (char**)realloc(*words, (*cnt_words + 1) * sizeof(char*));
-    if (errno == ENOMEM) {
+    if (new_array == NULL) {
         return BAD_ALLOC;
     }
     *words = new_array;
